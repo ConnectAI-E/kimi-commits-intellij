ba675b78054e6626a38a0bb3324400fd69579d06
river <laolei@forkway.cn>
2024-01-24 22:37:32 +0800

    feat: update commit icons

--- src/main/kotlin/com/github/connectai_e/kimi/commits/intellij/Icons.kt
+++ src/main/kotlin/com/github/connectai_e/kimi/commits/intellij/Icons.kt
@@ -3,5 +3,5 @@ package com.github.connectai_e.kimi.commits.intellij
 import com.intellij.openapi.util.IconLoader
 
 object Icons {
-    val AI_COMMITS = IconLoader.getIcon("/icons/aiCommits15.svg", javaClass)
+    val AI_COMMITS = IconLoader.getIcon("/icons/kimi-commit-logos.svg", javaClass)
 }
